bd8fa2f08beeee46d409b6ded5a9e55ac7663e65
Karankumar Nevage <111281049+Karan-nevage@users.noreply.github.com>
2025-07-23 21:59:08 +0530

    SEQUENCER

---
+++ fa_sqr.sv
@@ -0,0 +1,9 @@
+// ======================== Full Adder Sequencer ==================================
+
+class fa_sqr extends uvm_sequencer#(fa_tx);
+    `uvm_component_utils(fa_sqr)
+    `NEW_COMP
+endclass
+
+//===============================================================================
+// This file contains the sequencer for the full adder component
