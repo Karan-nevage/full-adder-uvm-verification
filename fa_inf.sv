ab6b788cf7f252b3a8dc771ccfa10f5c80ed7946
Karankumar Nevage <111281049+Karan-nevage@users.noreply.github.com>
2025-07-23 22:00:55 +0530

    INTERFACE

---
+++ fa_inf.sv
@@ -0,0 +1,12 @@
+// ========================= Full Adder Interface ==============================  
+
+interface fa_intf();
+    logic a;
+    logic b;
+    logic cin;
+    logic sum;
+    logic cout;
+endinterface
+
+//===============================================================================
+// This file contains the interface for the full adder component
